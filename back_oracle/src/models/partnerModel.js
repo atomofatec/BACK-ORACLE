cf8bbcd23ba00c4bfed2f40dbf5c630c4ed8354c
João Gabriel Ribeiro Mira <103254343+JoaoGRMira@users.noreply.github.com>
2024-04-08 18:38:07 -0300

    ⚡ perf: Remove hash
    Encriptação removida temporariamente

--- back_oracle/src/models/partnerModel.js
+++ back_oracle/src/models/partnerModel.js
@@ -7,11 +7,11 @@ const Partner = {};
 Partner.create = async (name, email, password) => {
     try {
         // Hash da senha
-        const hashedPassword = await bcrypt.hash(password, 10);
+        //const hashedPassword = await bcrypt.hash(password, 10);
 
         // Consulta SQL para inserir o usuário
         const query = 'INSERT INTO users (user_name, email, password, type, benefits, track_id, expertise_id) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *';
-        const values = [name, email, hashedPassword, 'parceiro', false, 1, 1];
+        const values = [name, email, password, 'parceiro', false, 1, 1];
 
         // Executar a consulta
         const result = await sql.query(query, values);
