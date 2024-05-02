90f2f11b3073038ff11e9e92f78fcbe5e8702b62
João Gabriel Ribeiro Mira <103254343+JoaoGRMira@users.noreply.github.com>
2024-05-02 18:54:18 -0300

    :bug: fix: database
    database para a aws

--- back_oracle/src/db/index.js
+++ back_oracle/src/db/index.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const connection = new Pool({
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
+    host: "localhost",
+    user: "postgres",
+    password: "fatec",
+    database: "oracle_db_v2",
 });
 
 module.exports = connection;
